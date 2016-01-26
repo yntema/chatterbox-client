5566380d94c3100c26fbc1c0247ac1fdb14d065a
yntema <gerrityntema@gmail.com>
2016-01-25 19:43:16 -0800

    Implement GET and POST functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,73 @@
-// YOUR CODE HERE:
 
+
+
+var app = {
+  init: function(){
+    $(document).ready(function(){
+      app.fetch();
+      $('#refresh').on('click', function(event){
+        $('#chats').children().remove();
+        app.fetch();
+      });
+      $('#submit').on('click', function() {
+        app.send();
+      });
+    });
+  },
+
+  fetch: function(){
+
+    $.ajax(
+    {
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      data: {"order":"-updatedAt"},
+      success: function(info) {
+
+        var data = info.results;
+        var rooms = {};
+
+        data.forEach( function(datum){
+          if(rooms[datum.roomname] !== undefined) {
+            rooms[datum.roomname].push(datum);
+          } else {
+            rooms[datum.roomname] = [];
+            rooms[datum.roomname].push(datum);
+          }
+          datum.text = xssFilters.inHTMLData(datum.text);
+          datum.username = xssFilters.inHTMLData(datum.username);
+          var $chatDiv = `<div chat><div class="username">${datum.username}</div><div class="createdAt">${datum.createdAt}</div><div class="text">${datum.text}</div></div>`
+
+          $("#chats").append(`<div chat><div class="username">${datum.username}</div>
+            <div class="createdAt">${datum.createdAt}</div>
+            <div class="text">${datum.text}</div></div>`);
+        });
+
+        for(var key in rooms){
+          $('select').append(`<option value ="${key}">${key}</option>`);
+        }
+      }
+    });
+  },
+
+  send: function(){       
+    var username = window.location.search.split("=")[1];
+    var $form = $('input');
+    var $option = $('select');
+    var dataPacket = {
+      roomname: $option.val(),
+      text: $form[0].value,
+      username: username
+    };
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(dataPacket),
+      success: function(){
+        console.log("Posted!");
+      }
+    })
+  }
+};
+
+app.init();
